0829d6770f22fb83519aaa22dde3a1dd131a8b7a
Elizabeth <elizabethhhauser06@gmail.com>
2017-06-29 19:40:22 +0000

    Dawg it up

--- Dawgshop/script.js
+++ Dawgshop/script.js
@@ -1,6 +1,6 @@
 document.getElementById('go').onclick = function() {
-var input text = $('#text-input').val()
-var dawgifiedText = inputText + ", dawg";
+var inputText = $('#text-input').val()
+var dawgifiedText = inputText + ", you are now in the future! But, you still have a problem when it comes to potatoes. Don't worry you are not alone. Practically everyone is as crazy about potatoes as you are. Have fun eating your potatoes, without me.... :(  ;)";
 document.getElementById('output').innerHTML = dawgifiedText;
 
 };
